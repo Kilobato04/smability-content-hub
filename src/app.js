f8a10fe1f3ce8c730decd45ae7b1b4e4c0ac0367
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2026-04-06 15:25:49 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -339,8 +339,8 @@ function buildSlideEl(slide, index, total, sidepx, bgBase64) {
                         <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://smability.io/aire/gpt.html" style="width:100%;">
                     </div>
                     <div>
-                        <p style="color:#fff; font-weight:800; font-size:20px; margin-bottom:5px;">AIreGPT v2.6</p>
-                        <p style="color:#9A9A9A; font-weight:600; font-size:16px;">Escanea para probar el monitoreo en tiempo real.</p>
+                        <p style="color:#fff; font-weight:800; font-size:20px; margin-bottom:5px;">AIreGPT</p>
+                        <p style="color:#9A9A9A; font-weight:600; font-size:16px;">Escanea para conocer tu exposición en tiempo real.</p>
                     </div>
                 </div>
             </div>`;
