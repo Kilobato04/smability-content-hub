6bdcb45ad196323a4e97e6a6ad3b63cd4cf60f0f
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2026-05-13 14:08:52 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -333,7 +333,7 @@ function buildSlideEl(slide, index, total, sidepx, bgBase64) {
                     </h3>
                 </div>
 
-               <div style="position:absolute; top:${deviceCircleSize * 0.8}px; right:-15px; text-align:right; z-index:13;">
+               <div style="position:absolute; top:${deviceCircleSize * 1.5}px; right:-15px; text-align:right; z-index:13;">
                     <span style="font-family:'Space Grotesk'; font-size:11px; color:#000; background:#39FF14; padding:3px 10px; font-weight:900; letter-spacing:1px; text-transform:uppercase; border-radius:2px; box-shadow: 5px 5px 15px rgba(0,0,0,0.4);">
                         ${deviceName}
                     </span>
